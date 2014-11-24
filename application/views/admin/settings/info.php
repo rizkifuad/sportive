06b10c16efbe219bda4aad7f9f6b4c4475e75447
Mustofa Kamal <mustovakamal@gmail.com>
2014-11-24 16:36:10 +0700

    form pengaturan - info : update 1

--- application/views/admin/settings/info.php
+++ application/views/admin/settings/info.php
@@ -1,1 +1,72 @@
-asd
+<div class="col-md-6">
+	<div class="box box-primary">
+		<div class="box-header">
+            <h3 class="box-title">Info lapangan</h3>
+        </div>
+    	<div class="box-body">
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Tambah lapangan</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Nama tempat</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Alamat lapangan</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Provinsi</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Kota</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Telepon</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	</div>
+   	 	<div class="box-footer">
+    	    <button type="submit" class="btn btn-primary">Simpan</button>
+    	</div>
+	</div>
+</div>
+<div class="col-md-6">
+	<div class="box box-primary">
+		<div class="box-header">
+            <h3 class="box-title">Info Pemilik</h3>
+        </div>
+    	<div class="box-body">
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Nama pemilik</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Email pemilik</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Telepon pemilik</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Username</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Password</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	    <div class="form-group">
+    	        <label for="jml_lapangan">Konfirmasi Password</label>
+    	        <input type="text" class="form-control" id="jml_lapangan" placeholder="Masukkan nama lapangan">
+    	    </div>
+    	</div>
+   	 	<div class="box-footer">
+    	    <button type="submit" class="btn btn-primary">Simpan</button>
+    	</div>
+	</div>
+</div>
